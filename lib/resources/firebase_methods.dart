5538d4b8bc1b2d81de74a3ea206486628aa19ea4
Kamil Lachowicz <kamil109111@gmail.com>
2020-12-16 01:06:47 +0100

    Part 4 - Getting Started With Layouts

--- lib/resources/firebase_methods.dart
+++ lib/resources/firebase_methods.dart
@@ -56,4 +56,10 @@ class FirebaseMethods {
         .document(currentUser.uid)
         .setData(user.toMap(user));
   }
+
+  Future<void> signOut() async {
+    await _googleSignIn.disconnect();
+    await _googleSignIn.signOut();
+    return await _auth.signOut();
+  }
 }
--- lib/resources/screens/login_screen.dart
+++ lib/resources/screens/login_screen.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:skype_clone/resources/firebase_repository.dart';
 import 'home_screen.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:skype_clone/resources/utils/universal_variables.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -11,26 +13,51 @@ class LoginScreen extends StatefulWidget {
 class LoginScreenState extends State<LoginScreen> {
   FirebaseRepository _repository = FirebaseRepository();
 
+  bool isLoginPressed = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: loginButton(),
+      backgroundColor: UniversalVariables.blackColor,
+      body: Stack(
+        children: [
+          Center(
+            child: loginButton(),
+          ),
+          isLoginPressed
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Container()
+        ],
+      ),
     );
   }
 
   Widget loginButton() {
-    return FlatButton(
-      padding: EdgeInsets.all(35),
-      child: Text(
-        "LOGIN",
-        style: TextStyle(
-            fontSize: 35, fontWeight: FontWeight.w900, letterSpacing: 1.2),
+    return Shimmer.fromColors(
+      baseColor: Colors.white,
+      highlightColor: UniversalVariables.senderColor,
+      child: FlatButton(
+        padding: EdgeInsets.all(35),
+        child: Text(
+          "LOGIN",
+          style: TextStyle(
+              fontSize: 35, fontWeight: FontWeight.w900, letterSpacing: 1.2),
+        ),
+        onPressed: () => performLogin(),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       ),
-      onPressed: () => performLogin(),
     );
   }
 
   void performLogin() {
+    print("tring to perform login");
+
+    setState(() {
+      isLoginPressed = true;
+    });
+
     _repository.signIn().then((FirebaseUser user) {
       if (user != null) {
         authenticateUser(user);
@@ -42,6 +69,10 @@ class LoginScreenState extends State<LoginScreen> {
 
   void authenticateUser(FirebaseUser user) {
     _repository.authenticateUser(user).then((isNewUser) {
+      setState(() {
+        isLoginPressed = false;
+      });
+
       if (isNewUser) {
         _repository.addDataToDb(user).then((value) {
           Navigator.pushReplacement(context,
