9d2d752c4ade2e8a1776a22a4cc0eac1429f4a4e
Kamil Lachowicz <kamil109111@gmail.com>
2020-12-29 23:16:51 +0100

    Part - 8 - Exchanging Messages Via Firebase

--- lib/resources/firebase_methods.dart
+++ lib/resources/firebase_methods.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:skype_clone/models/message.dart';
 import 'package:skype_clone/models/user.dart';
 import 'package:skype_clone/resources/utils/utilities.dart';
 
@@ -74,5 +75,22 @@ class FirebaseMethods {
       }
     }
     return userList;
+  }
+
+  Future<void> addMessageToDb(
+      Message message, User sender, User receiver) async {
+    var map = message.toMap();
+
+    await firestore
+        .collection("messages")
+        .document(message.senderId)
+        .collection(message.receiverId)
+        .add(map);
+
+    return await firestore
+        .collection("messages")
+        .document(message.receiverId)
+        .collection(message.senderId)
+        .add(map);
   }
 }
